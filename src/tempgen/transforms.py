7289dbaf1d69956a86101c113f9155a4f3798dfe
k5md <serg.dfc@yandex.ru>
2021-08-26 23:47:17 +0300

    add docstrings for transforms

--- src/tempgen/transforms.py
+++ src/tempgen/transforms.py
@@ -14,6 +14,18 @@ def ru_monetary_ending_append(value, *args):
     return '%s %s' % (value, monetary_ending)
 
 class Transforms():
+    """Class contains dictionary of function names and functions to be used in "pre" and "post" objects as "fn" property value in templates
+
+    Attributes
+    ----------
+    name_transform_map : Dict[str, callable]
+        Dictionary containing supported "functions" allowed in "pre" and "post" objects as "fn" property value in templates, uses function name as key and
+        function itself as value, these functions are applied on entry value.
+        Value argument is passed automatically, additional arguments to these functions are passed as "args" array.
+        Transform functions are executed sequentially, with value from prefious transform being passed to the next one.
+        Transforms used in "pre" object are applied to value from template entry when template is loaded, tempgen fields will contain already transformed value
+        Transforms used in "post" object are applied to value taken from tempgen fields only when generated document is being saved, tempgen fields will not be affected
+    """
     def __init__(self):
         self.name_transform_map = {
             'append': lambda value, postfix, *args: str(value) + postfix,
